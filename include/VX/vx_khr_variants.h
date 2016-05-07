22ee7581c885f5170bc8ae97f6279bb507fe163f
Tzu Hsiang Lin <thlin@pllab.cs.nthu.edu.tw>
2016-05-07 17:51:38 +0800

    Update from OpenVX svn r32782 | vladimir_dudnik | 2016-05-06 22:22:34 folder: include changes:  include/VX/vx_khr_variants.h -   Update comments for function behavior description

--- include/VX/vx_khr_variants.h
+++ include/VX/vx_khr_variants.h
@@ -49,11 +49,9 @@
  * In each of the cases a client of OpenVX could request the kernels in nearly
  * the same the same manner. There are two main approaches, which depend on the
  * method a client calls to get the kernel reference. The first uses enumerations.
- * \snippet examples/vx_kernels.c firstmethod
  * This method allows to client to attempt to find other targets and variants, but if
  * these are not present, the default node would still have been constructed.
  * The second method depends on using fully qualified strings to get the kernel reference.
- * \snippet examples/vx_kernels.c secondmethod
  * This second method is more compact but is does not permit fail-safing to default versions.
  *
  * As part of this extension, the function <tt>vxGetKernelByName</tt> will now accept more
